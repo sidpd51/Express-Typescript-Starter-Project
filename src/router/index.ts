6e516064f202aee37640eda6854a30b4b5a39b87
sidpd51 <sidpd510@gmail.com>
2025-04-12 12:32:37 +0530

    refactor: remove unnecessary middleware from ping router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,8 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
 import { pingController } from '../controllers/ping.controller'
 
 const pingRouter = express.Router();
 
-function middleware1(req: Request, res: Response, next: NextFunction) {
-    console.log("middleware 1");
-    next()
-    console.log("middleware 1 end");
-}
-
-function middleware2(req: Request, res: Response, next: NextFunction) {
-    console.log("middleware 2");
-    next()
-    console.log("middleware 2 end");
-}
-
-pingRouter.get('/ping', middleware1, middleware2, pingController);
+pingRouter.get('/ping', pingController);
 
 export default pingRouter;
